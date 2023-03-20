686334e1c18254664103ed012fee54ec51e88807
Shkurko Oksana <shkurko.oksana@gmail.com>
2023-03-20 15:31:21 +0700

    Create project with first empty activity

---
+++ app/src/main/java/com/example/healthymealplanner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.healthymealplanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
